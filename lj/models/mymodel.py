b8b245ae446892b8eb4ebefb2b11394f6ba59b44
regenalgrant <regenal@mac.com>
2016-12-28 14:26:51 -0800

    remove directory issues git hub

---
+++ lj/models/mymodel.py
@@ -0,0 +1,22 @@
+from sqlalchemy import (
+    Column,
+    Index,
+    Integer,
+    Unicode,
+    Date,
+)
+
+from .meta import Base
+
+
+class Entry(Base):
+    __tablename__ = 'entries'
+    id = Column(Integer, primary_key=True)
+    title = Column(Unicode)
+    body = Column(Unicode)
+    category = Column(Unicode)
+    tags = Column(Unicode)
+    creation_date = Column(Date)
+
+
+Index('my_index', Entry.title, unique=True, mysql_length=255)
